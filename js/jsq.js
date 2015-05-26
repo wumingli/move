d086b514e78b72d98ae01e27f008eaf8f670e0a8
武明礼 <mingli@sina.cn>
2015-05-26 21:28:13 +0800

    add 20150206

--- js/jsq.js
+++ js/jsq.js
@@ -9,38 +9,38 @@ function houseToMoney() {
         total = 0,
         lixiMonth = 0,
         needReturn = 0;
+    if ($.fn.Checkable) {
+        $('input.fd-radio').Checkable({
+            color: 'blue'
+        });
+    }
+    $('input[name="daikuan"]').on('change', function () {
+        $('#lilv-rate_input').val($('[value="' + $(this).val() + '"]').text());
+        $('#lilv-rate').val($(this).val());
+        lilv = parseFloat($('#lilv-rate').val());
+    });
     $('#fsq-rate').selectbox({
-        change: function() {
-            rate = $(this).attr('id').match(/\d+\.\d+/g);
-            if (rate !== null) {
-                rate = parseFloat(rate[0]);
-            }
+        change: function () {
+            rate = parseFloat($('#fsq-rate').val());
         }
     });
     $('#fd-month').selectbox({
-        change: function() {
-            month = $(this).attr('id').match(/\d+/g);
-            if (month !== null) {
-                month = parseFloat(month[0]);
-            }
-            console.log(month)
+        change: function () {
+            month = parseFloat($('#fd-month').val());
         }
     });
     $('#lilv-rate').selectbox({
-        change: function() {
-            lilv = $(this).attr('id').match(/\d+\.\d+/g);
-            if (lilv !== null) {
-                lilv = parseFloat(lilv[0]);
-            }
+        change: function () {
+            lilv = parseFloat($('#lilv-rate').val());
         }
     });
     //购房能力
     $('#gfnl-years').selectbox({
-        change: function() {
+        change: function () {
             console.log($('#gfnl-years').val());
         }
     });
-    $('#fsq-btn').on('click', function() {
+    $('#fsq-btn').on('click', function () {
         if (!/^\d+$/.test($acount.val())) {
             alert('贷款总额必须为正整数！');
             return false;
@@ -69,20 +69,20 @@ function originCountry() {
         amount = 100,
         $amount = $('.sum-have').find('input');
     $('#i-have').selectbox({
-        change: function() {
+        change: function () {
             var id = $(this).attr('id');
             from = id.substring(id.lastIndexOf('_') + 1);
             $('.sum-have-type').text(type[from]);
         }
     });
     $('#i-want').selectbox({
-        change: function() {
+        change: function () {
             var id = $(this).attr('id');
             to = id.substring(id.lastIndexOf('_') + 1);
             $('.sum-cash-type').text(type[to]);
         }
     });
-    $('#huilv-btn').on('click', function() {
+    $('#huilv-btn').on('click', function () {
         if (!/^\d+$/.test($amount.val())) {
             alert('持有金额必须为正整数！');
             return false;
@@ -96,7 +96,7 @@ function originCountry() {
                 to: to,
                 amount: amount
             },
-            success: function(data) {
+            success: function (data) {
                 if (data.status == 1) {
                     $('.sum-cash input').val(data.data);
                 }
@@ -109,64 +109,55 @@ originCountry();
 
 
 function showJSQ($obj) {
-    var id = $obj.attr('id');
-    $('.jsq-container').show();
-    $('.jsq-container .jsq-con').each(function() {
-        if ($(this).data('jsq-type') === id) {
-            $(this).show().siblings('.jsq-con').hide();
-            return;
-        }
-    });
-}
-//
-$('.count-list li, .jsq-selector a').on('click', function() {
+        var id = $obj.attr('id');
+        $('.jsq-container').show();
+        $('.jsq-container .jsq-con').each(function () {
+            if ($(this).data('jsq-type') === id) {
+                $(this).show().siblings('.jsq-con').hide();
+                return;
+            }
+        });
+    }
+    //
+$('.count-list li, .jsq-selector a').on('click', function () {
     showJSQ($(this));
 });
 //关闭
-$('.jsq-container .s-title em').on('click', function() {
+$('.jsq-container .s-title em').on('click', function () {
     $(this).parents('.jsq-container').hide();
 });
 //});
 
 //投资回报率计算器
-$("#touzistart").click(function() {
-    var touzizongjia = $("#touzizongjia").val().replace(/(^\s*)/g, "");
-    var touzizujin = $("#touzizujin").val().replace(/(^\s*)/g, "");
-
-    var regx = new RegExp("^(([0-9]+\.[0-9]*[1-9][0-9]*)|([0-9]*[1-9][0-9]*\.[0-9]+)|([0-9]*[1-9][0-9]*))$");
-    var regMonth = /^[1-9]\d*$/;
-
-    if (touzizongjia == "") {
+$('#zymbg').selectbox({
+    change: function () {
+        month = parseFloat($('#zymbg').val());
+    }
+});
+$("#touzistart").click(function () {
+    var gnsz = $("#gnsz").val().replace(/(^\s*)/g, "");
+    var reg = /^[1-9]\d*$/;
+    if (gnsz == "") {
         alert("购房总价不能为空");
-        $("#touzizongjia").focus();
+        $("#gnsz").focus();
         return false;
     }
-    if (!regMonth.test(touzizongjia)) {
+    if (!reg.test(gnsz)) {
         alert("购房总价金额不正确");
-        $("#touzizongjia").focus();
-        return false;
-    }
-    if (touzizujin == "") {
-        alert("每月租金费不能为空");
-        $("#touzizujin").focus();
-        return false;
-    }
-    if (!regMonth.test(touzizujin)) {
-        alert("每月租金费金额不正确");
-        $("#touzizujin").focus();
+        $("#gnsz").focus();
         return false;
     }
 
-    $("#touzijieguo").text(((touzizujin * 12) / touzizongjia * 100).toFixed(2));
+    $("#touzijieguo").text(gnsz * 10000 * 0.7);
 
     return false;
 });
 //客户购房能力评估
-$("#nlstart").click(function() {
+$("#nlstart").click(function () {
     var zijin = $("#zijin").val().replace(/(^\s*)/g, "");
     var shouru = $("#shouru").val().replace(/(^\s*)/g, "");
     var zhichu = $("#zhichu").val().replace(/(^\s*)/g, "");
-    var mianji = $("#mianji").val().replace(/(^\s*)/g, "");
+    //var mianji = $("#mianji").val().replace(/(^\s*)/g, "");
     var regx = new RegExp("^(([0-9]+\.[0-9]*[1-9][0-9]*)|([0-9]*[1-9][0-9]*\.[0-9]+)|([0-9]*[1-9][0-9]*))$");
     var regMonth = /^[1-9]\d*$/;
 
@@ -201,7 +192,7 @@ $("#nlstart").click(function() {
         return false;
     }
 
-    if (mianji == "") {
+    /*if (mianji == "") {
         alert("请填写计划购买的房屋面积");
         $("#mianji").focus();
         return false;
@@ -210,7 +201,7 @@ $("#nlstart").click(function() {
         alert("房屋面积不正确");
         $("#mianji").focus();
         return false;
-    }
+    }*/
 
 
     //您可购买的房屋总价=（家庭月收入-家庭月固定支出）×( (（1＋月利率）＾还款月数)－1  )÷［月利率×（1＋月利率）＾还款月数］+持有资金 
@@ -222,14 +213,14 @@ $("#nlstart").click(function() {
     js00 = parseFloat(zijin); //现持有
     js01 = parseFloat(shouru); //月收入
     js02 = parseFloat(zhichu); //月支出
-    js03 = parseFloat(mianji); //面积
+    //js03 = parseFloat(mianji); //面积
 
     var d1 = js01 - js02;
     var d2 = Math.pow(1 + lilu, month) - 1;
     var d3 = lilu * Math.pow(1 + lilu, month)
-    var dj = (parseFloat(Math.round(((d1 * d2) / d3) + js00)) / js03).toFixed(2);
+        //var dj = (parseFloat(Math.round(((d1 * d2) / d3) + js00)) / js03).toFixed(2);
     $("#zongjia").text(Math.round(((d1 * d2) / d3) + js00));
-    $("#danjia").text(dj);
+    //$("#danjia").text(dj);
     return false;
 });
 //海外房贷计算器
@@ -240,26 +231,26 @@ var arrayen = new Array("中国银行,3.89");
 var arrayja = new Array("中国银行,2.5");
 //海外房贷
 $('#hwfd-country').selectbox({
-    change: function() {
+    change: function () {
         var array_bank;
         var country = $("#hwfd-country").val();
         $("#loadYearRate").val('');
         switch (country) {
-            case "美国":
-                array_bank = arrayusa;
-                break;
-            case "澳洲":
-                array_bank = arrayau;
-                break;
-            case "加拿大":
-                array_bank = arrayca;
-                break;
-            case "英国":
-                array_bank = arrayen;
-                break;
-            case "新西兰":
-                array_bank = arrayja;
-                break;
+        case "美国":
+            array_bank = arrayusa;
+            break;
+        case "澳洲":
+            array_bank = arrayau;
+            break;
+        case "加拿大":
+            array_bank = arrayca;
+            break;
+        case "英国":
+            array_bank = arrayen;
+            break;
+        case "新西兰":
+            array_bank = arrayja;
+            break;
         };
 
         $("#hwfd-bank").html("<option>请选择银行</option>");
@@ -271,7 +262,7 @@ $('#hwfd-country').selectbox({
                 $("#hwfd-bank").append("<option value='" + _value.split(',')[1] + "'>" + _value.split(',')[0] + "</option>");
             }
             $("#hwfd-bank").selectbox({
-                change: function() {
+                change: function () {
                     var bank = $('#hwfd-bank').val();
                     $("#loadYearRate").val(bank.split(",")[0]);
                 }
@@ -281,7 +272,7 @@ $('#hwfd-country').selectbox({
 });
 $('#hwfd-bank').selectbox();
 
-$("#cal").click(function() {
+$("#cal").click(function () {
     if (CheckVal()) {
         ext_total();
     }
@@ -291,44 +282,44 @@ $("#cal").click(function() {
 var loadMoney, loadDate, loadYearRate;
 
 function CheckVal() {
-    loadMoney = $("#loadMoney").val().replace(/(^\s*)/g, "");
-    loadDate = $("#loadDate").val().replace(/(^\s*)/g, "");
-    loadYearRate = $("#loadYearRate").val().replace(/(^\s*)/g, "");
-    var regx = new RegExp("^(([0-9]+\.[0-9]*[1-9][0-9]*)|([0-9]*[1-9][0-9]*\.[0-9]+)|([0-9]*[1-9][0-9]*))$");
-    var regMonth = /^[1-9]\d*$/;
-    if (loadMoney == "") {
-        alert("贷款金额不能为空");
-        $("#loadMoney").focus();
-        return false;
+        loadMoney = $("#loadMoney").val().replace(/(^\s*)/g, "");
+        loadDate = $("#loadDate").val().replace(/(^\s*)/g, "");
+        loadYearRate = $("#loadYearRate").val().replace(/(^\s*)/g, "");
+        var regx = new RegExp("^(([0-9]+\.[0-9]*[1-9][0-9]*)|([0-9]*[1-9][0-9]*\.[0-9]+)|([0-9]*[1-9][0-9]*))$");
+        var regMonth = /^[1-9]\d*$/;
+        if (loadMoney == "") {
+            alert("贷款金额不能为空");
+            $("#loadMoney").focus();
+            return false;
+        }
+        if (!regMonth.test(loadMoney)) {
+            alert("贷款金额不正确");
+            $("#loadMoney").focus();
+            return false;
+        }
+        if (loadDate == "") {
+            alert("贷款期限不能为空");
+            $("#loadDate").focus();
+            return false;
+        }
+        if (!regMonth.test(loadDate)) {
+            alert("贷款期限不正确");
+            $("#loadDate").focus();
+            return false;
+        }
+        if (loadYearRate == "") {
+            alert("年利率不能为空");
+            $("#loadYearRate").focus();
+            return false;
+        }
+        if (!regx.test(loadYearRate)) {
+            alert("年利率不正确");
+            $("#loadYearRate").focus();
+            return false;
+        }
+        return true;
     }
-    if (!regMonth.test(loadMoney)) {
-        alert("贷款金额不正确");
-        $("#loadMoney").focus();
-        return false;
-    }
-    if (loadDate == "") {
-        alert("贷款期限不能为空");
-        $("#loadDate").focus();
-        return false;
-    }
-    if (!regMonth.test(loadDate)) {
-        alert("贷款期限不正确");
-        $("#loadDate").focus();
-        return false;
-    }
-    if (loadYearRate == "") {
-        alert("年利率不能为空");
-        $("#loadYearRate").focus();
-        return false;
-    }
-    if (!regx.test(loadYearRate)) {
-        alert("年利率不正确");
-        $("#loadYearRate").focus();
-        return false;
-    }
-    return true;
-}
-//本息还款的月还款额(参数: 年利率/贷款总额/贷款总月份)
+    //本息还款的月还款额(参数: 年利率/贷款总额/贷款总月份)
 function getMonthMoney1(lilv, total, month) {
     var lilv_month = lilv / 12; //月利率
     return total * lilv_month * Math.pow(1 + lilv_month, month) / (Math.pow(1 + lilv_month, month) - 1);
@@ -337,7 +328,7 @@ function getMonthMoney1(lilv, total, month) {
 function ext_total() {
     var month = $("#loadDate").val();
     var lilv = $("#loadYearRate").val() / 100;
-    var daikuan_total = $("#loadMoney").val();
+    var daikuan_total = $("#loadMoney").val() * 10000;
     var daikuantotal = daikuan_total;
     var month_money1 = getMonthMoney1(lilv, daikuantotal, month);
     $("#monthPayMoney").text(Math.round(month_money1));
--- js/main.js
+++ js/main.js
@@ -4,24 +4,24 @@
  * @date    2015-04-15 21:22:34
  * @version $Id$
  */
-$(function() {
+$(function () {
     if ($('.ck-slide').length !== 0) {
         $('.ck-slide').ckSlide({
             autoPlay: true
         });
     }
     //导航
-    $('.hasSubNav').hover(function() {
+    $('.hasSubNav').hover(function () {
         $(this).find('.subnav').show();
-    }, function() {
+    }, function () {
         $(this).find('.subnav').hide();
     });
     //目标国推荐
-    $('.country li').click(function() {
+    $('.country li').click(function () {
         var $img = $(this).find('img'),
             index = $img.data('index');
         $img.attr('src', $(this).find('img').data('hover-src'));
-        $(this).siblings().each(function() {
+        $(this).siblings().each(function () {
             $(this).find('img').attr('src', $(this).find('img').data('origin-src'));
         });
         $('.country-container').eq(parseInt(index)).show().siblings().hide();
@@ -29,18 +29,18 @@ $(function() {
 
     //目标国推荐小图切换
     $('.tz-left ul li a').attr('href', 'javascript:void(0);');
-    $('.tz-left ul li').on('click', function() {
+    $('.tz-left ul li').on('click', function () {
         $(this).parents('.tz-left').find('.tz-left-img img').attr('src', $(this).find('img').attr('src'));
         return false;
     });
 
     //调查Ajax
-    $('.move-survey').on('submit', function() {
-        /*if ($.trim($('[name="contact"]').val()) === '') {
-            alert('请输入您的联系方式');
-            $('[name="contact"]').focus();
-            return false;
-        }*/
+    $('[name="contact"]').focus(function () {
+        if ($(this).val() === '选填') {
+            $(this).val('');
+        }
+    });
+    $('.move-survey').on('submit', function () {
         if ($.trim($('[name="user_name"]').val()) === '') {
             alert('请输入您的姓名');
             $('[name="user_name"]').focus();
@@ -55,23 +55,26 @@ $(function() {
             $('[name="user_email"]').select();
             return false;
         }
+        if ($.trim($('[name="contact"]').val()) === '选填') {
+            $('[name="contact"]').val('')
+        }
         $.ajax({
             url: 'index.php?m=Survey&a=setInfo', //这里改成调查的URL
             data: $('.move-survey').serialize(),
             dataType: 'json',
-            success: function(data) {
+            success: function (data) {
                 if (data.status == 1) {
                     alert('问卷提交成功，感谢您的参与');
                 } else {
                     alert('问卷提交失败');
                 }
-                
+
             }
         });
         return false;
     });
     //快速咨询您感兴趣的房源
-    $('.consult-form').on('submit', function() {
+    $('.consult-form').on('submit', function () {
         if ($.trim($('[name="user_name"]').val()) === '') {
             alert('请输入您的姓名');
             $('[name="user_name"]').focus();
@@ -99,7 +102,7 @@ $(function() {
             url: 'index.php?m=Survey&a=setMessage', //这里改成调查的URL
             data: $('.consult-form').serialize(),
             dataType: 'json',
-            success: function(data) {
+            success: function (data) {
                 if (data.status == 1) {
                     alert('咨询提交成功');
                 } else {
@@ -110,31 +113,38 @@ $(function() {
         return false;
     });
     //投资列表中更多条件下拉选择移到这里
-    if ($.fn.Checkable) {
+    /*if ($.fn.Checkable) {
         $('input.fd-radio').Checkable({
             color: 'blue'
         });
-    }
+    }*/
     if ($.fn.selectbox) {
         $(".fd-select1").selectbox();
         $(".fd-select2").selectbox({
-            containerClass:"h_selectbox-wrapper",
-            hoverClass:"h_current",
-            selectedClass:"h_selected",
-            inputClass:"h_selectbox",
-            change: function(){
+            containerClass: "h_selectbox-wrapper",
+            hoverClass: "h_current",
+            selectedClass: "h_selected",
+            inputClass: "h_selectbox",
+            change: function () {
                 location.href = $(".fd-select2").val();
             }
         });
         $(".fd-select3").selectbox({
-            containerClass:"h_selectbox-wrapper",
-            hoverClass:"h_current",
-            selectedClass:"h_selected",
-            inputClass:"h_selectbox",
+            containerClass: "h_selectbox-wrapper",
+            hoverClass: "h_current",
+            selectedClass: "h_selected",
+            inputClass: "h_selectbox",
             //bindDiv:["select_tab1","select_tab2","select_tab3"],
-            change: function(){
+            change: function () {
                 location.href = $(".fd-select3").val();
             }
+        });
+    }
+    //首页投资列表Tab切换
+    if ($('.tz-nav').length > 0) {
+        $('.tz-nav li').on('click', function () {
+            $(this).addClass('active').siblings('li').removeClass('active');
+            $('.tz-box').removeClass('tz-box-active').eq($(this).index()).addClass('tz-box-active');
         });
     }
 });
